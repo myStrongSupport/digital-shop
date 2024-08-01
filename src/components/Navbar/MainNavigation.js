7d3f52cbdf0b63b3de9d857a6f3de3dd8a05c436
Hamed <hamedroaddark@gmail.com>
2024-08-01 16:46:56 +0330

    navigation responsive

--- src/components/Navbar/MainNavigation.js
+++ src/components/Navbar/MainNavigation.js
@@ -7,7 +7,13 @@ import { GrShop } from "react-icons/gr";
 import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "../../store/slices/ui_slice";
 import { userActions } from "../../store/slices/user-slice";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
+
 const MainNavigation = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [showMenu, setShowMenu] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
   const totalQuentity = useSelector((state) => state.cart.totalQuentity);
   const user = useSelector((state) => state.user);
@@ -20,6 +26,13 @@ const MainNavigation = () => {
   const onModalUserHandler = () => {
     dispatch(uiActions.toggleUser());
   };
+
+  // Functions
+  const toggleMenuHandler = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  // UseEffects
 
   useEffect(() => {
     const onGetCurrentUser = async () => {
@@ -45,6 +58,14 @@ const MainNavigation = () => {
     }
   }, [currentUser, dispatch]);
 
+  // Responsive
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <header className={classes.header}>
       <div className={classes.container}>
@@ -54,71 +75,161 @@ const MainNavigation = () => {
               <img src={LOGO} alt="A Shop Logo" />
             </Link>
           </div>
-          <nav>
-            <ul className={classes.list}>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? classes.active : undefined
-                  }
-                  to=""
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? classes.active : undefined
-                  }
-                  to="shop"
-                >
-                  Shop
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? classes.active : undefined
-                  }
-                  to="about"
-                >
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? classes.active : undefined
-                  }
-                  to="contact"
-                >
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
+          {/* Desktop */}
+          {width >= 768 && (
+            <nav className={classes.navbar}>
+              <ul className={classes.list}>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to=""
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to="shop"
+                  >
+                    Shop
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to="about"
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to="contact"
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          )}
+          {/* Menu */}
+          {width <= 768 && showMenu && (
+            <nav className={classes.navbar}>
+              <div className={classes["logo_res"]}>
+                <Link to="">
+                  <img src={LOGO} alt="A Shop Logo" />
+                </Link>
+                <MdOutlineClose
+                  size="2rem"
+                  onClick={toggleMenuHandler}
+                  className={classes.ham}
+                />
+              </div>
+              <ul className={classes.list}>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to=""
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to="shop"
+                  >
+                    Shop
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to="about"
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    to="contact"
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+                <li></li>
+              </ul>
+              <ul className={classes["login-part"]}>
+                <li className={classes.login}>
+                  {user.user ? (
+                    <div onClick={onModalUserHandler}>U</div>
+                  ) : (
+                    <Link to={"/sign_up"}>Sign up</Link>
+                  )}
+                </li>
+
+                <li className={classes.badge}>
+                  <Link to="cart">
+                    <span>{totalQuentity}</span>
+                    Your Badge
+                  </Link>
+                </li>
+                <li onClick={goSearchHandler}>
+                  <IoSearchOutline size="1.5rem" />
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
-        <div className={classes["login-part"]}>
-          <div className={classes.login}>
-            {user.user ? (
-              <div onClick={onModalUserHandler}>U</div>
-            ) : (
-              <Link to={"/sign_up"}>Sign up</Link>
-            )}
-          </div>
-          <div className={classes["badge-search-part"]}>
-            <div onClick={goSearchHandler}>
-              <IoSearchOutline />
+        {/* Search & Badge  */}
+        {width >= 768 && (
+          <div className={classes["login-part"]}>
+            <div className={classes.login}>
+              {user.user ? (
+                <div onClick={onModalUserHandler}>U</div>
+              ) : (
+                <Link to={"/sign_up"}>Sign up</Link>
+              )}
             </div>
-            <div className={classes.badge}>
-              <Link to="cart">
-                <span>{totalQuentity}</span>
-                <GrShop />
-              </Link>
+            <div className={classes["badge-search-part"]}>
+              <div onClick={goSearchHandler}>
+                <IoSearchOutline />
+              </div>
+              <div className={classes.badge}>
+                <Link to="cart">
+                  <span>{totalQuentity}</span>
+                  <GrShop />
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+        {width <= 768 && (
+          <HiMenuAlt2
+            size="2rem"
+            onClick={toggleMenuHandler}
+            className={classes.ham}
+          />
+        )}
       </div>
     </header>
   );

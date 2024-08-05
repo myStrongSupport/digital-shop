82dd4a62c681e4cbd2b9dac3f34f75b766ddc6d5
Hamed <hamedroaddark@gmail.com>
2024-08-05 11:26:09 +0330

    dublicate_recent_news

--- src/components/News/RecentNews.js
+++ src/components/News/RecentNews.js
@@ -34,6 +34,30 @@ const news = [
       "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos itaque aperiam odio qui tempore est soluta numquam iusto deserunt, ipsum doloremque quibusdam. Possimus repellat cupiditate, et error veniam temporibus vitae!",
     id: 3,
   },
+  {
+    author: "October 5 , 2020",
+    img: "https://i.pinimg.com/564x/2b/70/88/2b70888d5df774da0d1b87fa8c749107.jpg",
+    title: "How to use perfect gadget",
+    content:
+      "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos itaque aperiam odio qui tempore est soluta numquam iusto deserunt, ipsum doloremque quibusdam. Possimus repellat cupiditate, et error veniam temporibus vitae!",
+    id: 4,
+  },
+  {
+    author: "Des 5 , 2019",
+    img: "https://i.pinimg.com/564x/c9/be/c7/c9bec73e431c1e32dad6bc59f0f037c9.jpg",
+    title: "How to use perfect gadget",
+    content:
+      "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos itaque aperiam odio qui tempore est soluta numquam iusto deserunt, ipsum doloremque quibusdam. Possimus repellat cupiditate, et error veniam temporibus vitae!",
+    id: 5,
+  },
+  {
+    author: "Nov 18 , 2020",
+    img: "https://i.pinimg.com/564x/b1/a7/e2/b1a7e23efd345f622f5f6994ccc1b311.jpg",
+    title: "How to use perfect gadget",
+    content:
+      "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos itaque aperiam odio qui tempore est soluta numquam iusto deserunt, ipsum doloremque quibusdam. Possimus repellat cupiditate, et error veniam temporibus vitae!",
+    id: 6,
+  },
 ];
 const RecentNews = () => {
   return (
@@ -62,7 +86,7 @@ const RecentNews = () => {
             modules={[Grid, Pagination]}
           >
             {news.map((news) => (
-              <SwiperSlide>
+              <SwiperSlide key={news.id}>
                 <RecentNewsItem
                   key={news.id}
                   title={news.title}
